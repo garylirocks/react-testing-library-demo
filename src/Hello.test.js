d5d7a2c82fa80c51ca468f4dc7698bb76363fdb9
garylirocks <gary.li.rocks@gmail.com>
2020-03-09 12:00:59 +1300

    remove manually added jest, it was included by create-react-app already

--- src/Hello.test.js
+++ src/Hello.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, waitForElement, fireEvent } from "react-testing-library";
 import Hello from "./Hello";
 
-
 it("Renders correctly", async () => {
   // Render new instance in every test to prevent leaking state
   const { getByText } = render(<Hello name="Gary" />);
@@ -17,5 +16,5 @@ it("Document event listeners work", async () => {
   const textNode = await waitForElement(() => getByText(/Hola Gary/i));
   // expect(textNode.textContent).toBe("Hola Gary");
 
-  expect(textNode).toHaveTextContent('Hola Gary');
+  expect(textNode).toHaveTextContent("Hola Gary");
 });
